0839c09e4a703f210f0cb46cf47358a9baa40f23
Bobyshev Nikita <Gammanik@gmail.com>
2018-04-27 23:52:01 +1000

    1: catch intent.data in callback from webview

--- app/src/main/java/ya/co/yandex_gallery/LoginActivity.kt
+++ app/src/main/java/ya/co/yandex_gallery/LoginActivity.kt
@@ -1,12 +1,50 @@
 package ya.co.yandex_gallery
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import ya.co.yandex_gallery.util.AppConstants
 
 class LoginActivity : AppCompatActivity() {
+
+    val TAG = "LoginActivity"
+
+    @BindView(R.id.button_login) lateinit var loginButton: Button
+
+    @OnClick(R.id.button_login)
+    fun loginClick() { //todo: use active dialog onCreate instead
+        Log.d("Login", "clicked!")
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(AppConstants.AUTH_URL)))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ButterKnife.bind(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val uri = intent.data
+        intent = null
+
+        if (uri != null && uri.toString().startsWith(AppConstants.REDIRECT_URL)) {
+//            val accessToken = uri.getEncodedSchemeSpecificPart()
+//            Log.d(TAG, "access_token: $accessToken  and  ${uri.encodedPath}")
+            Log.d(TAG, "uri: $uri")
+            //todo: put token in preferences
+//          saveToken(accessToken)
+
+            Toast.makeText(this, "Welcome back, username!", LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/ya/co/yandex_gallery/util/AppConstants.kt
@@ -0,0 +1,14 @@
+package ya.co.yandex_gallery.util
+
+import ya.co.yandex_gallery.util.Env.CLIENT_ID
+
+
+class AppConstants {
+    companion object {
+        @JvmStatic
+        val REDIRECT_URL = "yandex-gallery://token-callback"
+
+        @JvmStatic //todo: add CALLBACK_URL as parameter here?
+        val AUTH_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URL"
+    }
+}
