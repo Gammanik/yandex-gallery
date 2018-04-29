99bb0459ff5b50f10342b76638c15c1bbb02a1c5
Bobyshev Nikita <Gammanik@gmail.com>
2018-04-29 12:17:11 +1000

    1: save token onLogin

--- app/src/main/java/ya/co/yandex_gallery/LoginActivity.kt
+++ app/src/main/java/ya/co/yandex_gallery/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
@@ -12,6 +13,7 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import ya.co.yandex_gallery.util.AppConstants
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -33,18 +35,34 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        onLogin()
+    }
 
+    private fun onLogin() {
         val uri = intent.data
         intent = null
 
         if (uri != null && uri.toString().startsWith(AppConstants.REDIRECT_URL)) {
-//            val accessToken = uri.getEncodedSchemeSpecificPart()
-//            Log.d(TAG, "access_token: $accessToken  and  ${uri.encodedPath}")
-            Log.d(TAG, "uri: $uri")
-            //todo: put token in preferences
-//          saveToken(accessToken)
 
-            Toast.makeText(this, "Welcome back, username!", LENGTH_SHORT).show()
+            Log.d(TAG, "uri: $uri")
+            val pattern = Pattern.compile("access_token=(.*?)(&|\$)")
+            val matcher = pattern.matcher(uri.toString())
+
+            if(matcher.find()) {
+                val accessToken = matcher.group(1)
+                Toast.makeText(this, "Welcome back, username!", LENGTH_SHORT).show()
+                Log.d(TAG, "access_token: $accessToken")
+                saveToken(accessToken)
+
+            } else {
+                Toast.makeText(this, "Registration token not found!", LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun saveToken(accessToken: String) {
+        val editor = PreferenceManager.getDefaultSharedPreferences(this).edit()
+        editor.putString(AppConstants.TOKEN_KEY, accessToken)
+        editor.apply()
     }
 }
--- app/src/main/java/ya/co/yandex_gallery/util/AppConstants.kt
+++ app/src/main/java/ya/co/yandex_gallery/util/AppConstants.kt
@@ -10,5 +10,8 @@ class AppConstants {
 
         @JvmStatic //todo: add CALLBACK_URL as parameter here?
         val AUTH_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URL"
+
+        @JvmStatic
+        val TOKEN_KEY = "ACCESS_TOKEN_KEY"
     }
 }
